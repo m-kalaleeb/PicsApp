ce8e4845b918d4268dcf1e420a500eb966bee998
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-08-06 00:41:40 +0200

    Adding mapper for photos

--- data/src/main/java/com/sixbits/data/mapper/AlbumsMapper.kt
+++ data/src/main/java/com/sixbits/data/mapper/AlbumsMapper.kt
@@ -1,14 +1,20 @@
 package com.sixbits.data.mapper
 
 import com.sixbits.domain.model.Album
-import com.sixbits.domain.response.AlbumsItemResponse
+import com.sixbits.domain.model.AlbumPhoto
+import com.sixbits.domain.response.AlbumItemResponse
+import com.sixbits.domain.response.AlbumPhotoItemResponse
 
-fun AlbumsItemResponse.toAlbum(): Album {
+fun AlbumItemResponse.toAlbum(): Album {
     return Album(
         id = id,
-        albumId = albumId,
         title = title,
-        url = url,
-        thumbnailUrl = thumbnailUrl
+        userId = userId
+    )
+}
+
+fun AlbumPhotoItemResponse.toAlbumPhoto(): AlbumPhoto {
+    return AlbumPhoto(
+        id, albumId, title, url, thumbnailUrl
     )
 }
