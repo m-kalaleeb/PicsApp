e72da68f7ebb5712a049212f048848d1b8f461d6
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-08-06 00:41:28 +0200

    Correcting API to match the server

--- domain/src/main/java/com/sixbits/domain/api/AlbumsApi.kt
+++ domain/src/main/java/com/sixbits/domain/api/AlbumsApi.kt
@@ -1,10 +1,15 @@
 package com.sixbits.domain.api
 
-import com.sixbits.domain.response.AlbumsItemResponse
+import com.sixbits.domain.response.AlbumItemResponse
+import com.sixbits.domain.response.AlbumPhotoItemResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface AlbumsApi {
 
-    @GET("albums/1/photos")
-    suspend fun getAlbums(): List<AlbumsItemResponse>
+    @GET("albums/{album_id}/photos")
+    suspend fun getAlbumsPhotos(@Path("album_id") id: Int): List<AlbumPhotoItemResponse>
+
+    @GET("albums")
+    suspend fun getAlbums(): List<AlbumItemResponse>
 }
