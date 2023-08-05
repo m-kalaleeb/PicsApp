cfb3720637e9ce6cc3d052f27b4a9c2f2c580723
Mohammad Al Kalaleeb <mohammadautumn@outlook.com>
2023-08-06 00:42:52 +0200

    Added mapper tests for the 2 responses mappers

--- data/src/test/java/com/sixbits/data/mapper/AlbumsMapperTest.kt
+++ data/src/test/java/com/sixbits/data/mapper/AlbumsMapperTest.kt
@@ -1,14 +1,15 @@
 package com.sixbits.data.mapper
 
-import com.sixbits.domain.response.AlbumsItemResponse
+import com.sixbits.domain.response.AlbumItemResponse
+import com.sixbits.domain.response.AlbumPhotoItemResponse
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class AlbumsMapperTest {
 
     @Test
-    fun testAlbumMapping() {
-        val albumResponse = AlbumsItemResponse(
+    fun testAlbumPhotosMapping() {
+        val albumResponse = AlbumPhotoItemResponse(
             id = 5,
             albumId = 7,
             url = "https://test.com",
@@ -16,12 +17,27 @@ class AlbumsMapperTest {
             title = "Amazing title"
         )
 
-        val albumItem = albumResponse.toAlbum()
+        val albumItem = albumResponse.toAlbumPhoto()
 
         assertEquals(5, albumItem.id)
         assertEquals(7, albumItem.albumId)
         assertEquals("https://test.com", albumItem.url)
         assertEquals("https://test.com/thumbnail", albumItem.thumbnailUrl)
+        assertEquals("Amazing title", albumItem.title)
+    }
+
+    @Test
+    fun testAlbumMapping() {
+        val albumResponse = AlbumItemResponse(
+            id = 5,
+            userId = 7,
+            title = "Amazing title"
+        )
+
+        val albumItem = albumResponse.toAlbum()
+
+        assertEquals(5, albumItem.id)
+        assertEquals(7, albumItem.userId)
         assertEquals("Amazing title", albumItem.title)
     }
 }
